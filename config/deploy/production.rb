616b6ceaf555e2f867a695216c01b46dd205ce78
Robert Houser <bobac@bobac.net>
2017-02-07 18:55:26 +0100

    Added db role for deployment

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 set :stage, :production
 
 # Replace 127.0.0.1 with your server's IP address!
-server '35.157.23.39', user: 'deploy', roles: %w{web app}
+server '35.157.23.39', user: 'deploy', roles: %w{web db app}
 
 # server-based syntax
 # ======================
