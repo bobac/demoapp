7dd6e517e46f0f3236bb418b6782acbc6cd1856f
Robert Houser <bobac@bobac.net>
2017-02-07 18:37:21 +0100

    changed server IP

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 set :stage, :production
 
 # Replace 127.0.0.1 with your server's IP address!
-server '35.157.119.217', user: 'deploy', roles: %w{web app}
+server '35.157.23.39', user: 'deploy', roles: %w{web app}
 
 # server-based syntax
 # ======================
